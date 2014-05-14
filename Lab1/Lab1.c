5e212058235d1077a0e107210202c5c6433f5f70
quinlanb <quinlanb@wit.edu>
2014-05-14 18:37:48 -0400

    Switch statement implemented, Sleep sorted

--- Lab1/Lab1.c
+++ Lab1/Lab1.c
@@ -1,10 +1,45 @@
 // Lab1.cpp : Defines the entry point for the console application.
-//
+//Bryan Quinlan
 
 #include "stdafx.h"
+#include <time.h>
+#include <stdlib.h>
+#include <dos.h>
+#include <stdio.h>
+#include <Windows.h>
 
-int main(int argc, char *argv[]) {
-	printf("Hello, World!\n");
+
+int main() {
+	int r = 0;
+	int exit = 0;
+
+	srand(time(NULL)); //seed random number
+	r = rand();
+
+	printf("CPU running normally, enjoying life as a computer");
+
+
+	while(exit = 0){
+
+		switch(r){
+		case 0:
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+			Sleep(2000);
+			//call_ISR(IV[r]);
+		default:
+			printf("Still chuggin' along");
+			break;
+		}
+
+	}
+
+
 
 	return 0;
 }
