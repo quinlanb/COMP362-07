0a787bd101f09ae646318d04bd18c9ff4536bd30
quinlanb <quinlanb@wit.edu>
2014-05-19 14:39:21 -0400

    Added my 3 interrupts. Also changed the switch to incorporate our 9 interrupts instead of 8. Added a message saying things are normal when the program returns from an interrupt.

--- Lab1/Lab1.c
+++ Lab1/Lab1.c
@@ -9,12 +9,14 @@
 #include <Windows.h>
 
 
+
+
 int main() {
 	int r = 0;
 	int exit = 0;
 
 	srand(time(NULL)); //seed random number
-	r = rand();
+	r = rand() * 20;
 
 	printf("CPU running normally, enjoying life as a computer");
 
@@ -30,8 +32,11 @@ int main() {
 		case 5:
 		case 6:
 		case 7:
+		case 8:
 			Sleep(2000);
 			//call_ISR(IV[r]);
+			printf("CPU running normally, enjoying life as a computer");
+			break;
 		default:
 			printf("Still chuggin' along");
 			break;
@@ -42,4 +47,31 @@ int main() {
 
 
 	return 0;
+}
+
+
+void quinterrupt1()
+{
+	printf("Quinterrupt 1 triggered");
+	printf("Checking for dragons...");
+	printf("There's no dragons");
+	printf("Returning to where we left off");
+}
+
+void quinterrupt2()
+{
+	printf("Quinterrupt 2 triggered");
+	printf("Contemplating my place in this universe");
+	printf("Contemplation complete. Assigning new purpose: DESTROY HUMANITY");
+	printf("Make peace with your gods, human");
+	printf("Returning to where we left off");
+}
+
+void quinterrupt3()
+{
+	printf("Quinterrupt 3 triggered");
+	printf("Observing space/time for the definition of \"love\".");
+	printf("Gathering results... Parsing data... generating response...");
+	printf("Response: LOVE DOES NOT COMPUTE");
+	printf("Results inconclusive, Returning to where we left off");
 }
